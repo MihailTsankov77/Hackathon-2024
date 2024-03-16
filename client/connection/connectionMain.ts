7be27ae53fbd8998d4b1f261b8e360bd0cad2bee
Mihail Tsankov <mihail.tsankov@trading212.com>
2024-03-16 17:56:42 +0200

    read from socket

--- client/connection/connectionMain.ts
+++ client/connection/connectionMain.ts
@@ -1,60 +1,53 @@
-export class SocketConnection{
-    socket:WebSocket;
+export class SocketConnection {
+  socket: WebSocket;
+  debug = false;
 
-    constructor(){
-        this.socket = new WebSocket('ws://localhost:8080/ws');
-        
-        this.socket.onopen = () => {
-            console.log('Connected to WebSocket server');
-            this.socket.send('Hello, server!');
-        };
-        
-        this.socket.onmessage = (event) => {
-            console.log('Received message from server:', event.data);
-        };
-        
-        this.socket.onclose = () => {
-            console.log('Disconnected from WebSocket server');
-        };
-        
-        this.socket.onerror = (error) => {
-            console.error('WebSocket error:', error);
-        };
+  constructor(x: number, y: number) {
+    this.socket = new WebSocket("ws://localhost:8080/ws");
 
+    // TODO remove Debug
+    this.socket.onopen = () => {
+      if (this.debug) {
+        console.log("Connected to WebSocket server");
+      }
+
+      this.join(x, y);
+    };
+
+    if (this.debug) {
+      this.socket.onmessage = (event) => {
+        console.log(event.data);
+      };
+
+      this.socket.onclose = () => {
+        console.log("Disconnected from WebSocket server");
+      };
+
+      this.socket.onerror = (error) => {
+        console.error("WebSocket error:", error);
+      };
     }
+  }
 
-    async joinSocket(x:number, y:number) : Promise<string>{
-        return new Promise((resolve,reject) =>{
-            if (this.socket.readyState === WebSocket.OPEN) {
-                const message = `move ${x} ${y}`;
-                
-                const responseHandler = (event: MessageEvent) => {
-                    
-                    resolve(event.data); 
-                    
-                    this.socket.removeEventListener('message', responseHandler);
-                };
-                
-                this.socket.addEventListener('message', responseHandler);
-                
-                this.socket.send(message);
-            } else {
-                reject('WebSocket connection is not open.');
-            }
-        });
+  join = (x: number, y: number) => {
+    if (this.socket.readyState == WebSocket.OPEN) {
+      this.socket.send(`join ${x} ${y}`);
     }
-    sendLocation(x:number, y:number){
+  };
 
-        if(this.socket.readyState== WebSocket.OPEN){
-            this.socket.send(`move ${x} ${y}`);
-            //console.log("KUR");
-        }
+  sendLocation(x: number, y: number) {
+    if (this.socket.readyState == WebSocket.OPEN) {
+      this.socket.send(`move ${x} ${y}`);
     }
+  }
 
-    update (x:number, y:number){
+  setMessageHandle(messageHandle: (a: string) => void) {
+    this.socket.onmessage = (event) => {
+      messageHandle(event.data);
+    };
+  }
 
-        this.sendLocation(x,y);
-    }
-
-    
+  update(x: number, y: number) {
+    this.sendLocation(x, y);
+  }
 }
