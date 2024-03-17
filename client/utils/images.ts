263de3883f33db95f4aaef268921561681276ebe
Mihail Tsankov <mihail.tsankov@trading212.com>
2024-03-17 09:38:15 +0200

    images

--- client/utils/images.ts
+++ client/utils/images.ts
@@ -2,9 +2,11 @@ import Phaser from "phaser";
 
 export function preloadImages(game: Phaser.Scene) {
   game.load.image("player", "player.png");
+  game.load.image("player-glow", "player-glow.png");
   game.load.image("slavi", "player_1.png");
+  game.load.image("slavi-glow", "player-green-glow.png");
   game.load.image("background", "grid.png");
-  game.load.image("hand", "hands_2.png");
+  game.load.image("hand", "hands.png");
   game.load.image("score", "kur.png");
   game.load.image("game-over", "game-over.png");
 }
